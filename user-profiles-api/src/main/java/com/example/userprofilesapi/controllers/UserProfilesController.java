49b65575280e4205392e4cb422f42d4975f94cbc
andy tham <thamandyy@gmail.com>
2018-06-13 12:33:44 -0400

    remove double cors

--- user-profiles-api/src/main/java/com/example/userprofilesapi/controllers/UserProfilesController.java
+++ user-profiles-api/src/main/java/com/example/userprofilesapi/controllers/UserProfilesController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 public class UserProfilesController {
 
